3333c0c506d9962ccbf272a2b6b1475d91416f4d
Thato Matlala <thatomatlala28@gmail.com>
2024-10-15 13:51:59 +0200

    allowed the application to run on any port

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def get_weather():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="127.0.0.1", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
